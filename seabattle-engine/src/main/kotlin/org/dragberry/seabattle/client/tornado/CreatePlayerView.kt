7a3bff2d70f5be3e3861a286bc358c4c5af6e9c1
Maksim Drahun <DRAHUN.MAXIM@standardbank.co.za>
2019-07-09 18:18:08 +0300

    GUI client

--- seabattle-engine/src/main/kotlin/org/dragberry/seabattle/client/tornado/CreatePlayerView.kt
+++ seabattle-engine/src/main/kotlin/org/dragberry/seabattle/client/tornado/CreatePlayerView.kt
@@ -1,6 +1,8 @@
 package org.dragberry.seabattle.client.tornado
 
+import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
 import javafx.beans.value.ChangeListener
 import javafx.beans.value.ObservableValue
 import javafx.scene.Parent
@@ -13,6 +15,7 @@ import kotlinx.coroutines.launch
 import org.dragberry.seabattle.engine.*
 import tornadofx.*
 import java.beans.PropertyChangeListener
+import java.lang.NumberFormatException
 import java.util.*
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
@@ -106,65 +109,75 @@ class CaptainCreateDialog : Fragment() {
 
     private var continuation: Continuation<Commander>? = null
 
-    val name = SimpleStringProperty()
+    private val nameProp = SimpleStringProperty()
 
-    override val root = form {
-        gridpane {
-            val cs1 = ColumnConstraints()
-            cs1.percentWidth = 50.0
-            val cs2 = ColumnConstraints()
-            cs2.percentWidth = 50.0
-            columnConstraints.addAll(cs1, cs2)
+    private val widthProp = SimpleStringProperty()
 
-            useMaxWidth = true
-            spacing = 10.0
+    private val heightProp = SimpleStringProperty()
 
-            row {
-                label("Player name:")
-                textfield(name)
-            }
-            row {
-                add(label("Settings:"), 0, 1, 2, 1)
-            }
-            row {
-                hbox {
-                    label("Width:")
-                    textfield()
-                }
-                hbox {
-                    label("Height:")
-                    textfield()
-                }
-            }
-            row {
-                spacing = 10.0
-                button("Confirm") {
-                    padding = insets(10.0)
-                    useMaxWidth = true
-                    action {
-                        continuation?.resume(Captain(
-                            object : CommanderController {
-                                override suspend fun getName(): String = name.get()
-                                override suspend fun getRole(): Boolean = Random.nextBoolean()
-                                override suspend fun getSettings(): BattleSettings =
-                                    BattleSettings(10, 10, listOf(4, 3, 2, 1))
+    override val root = gridpane {
+        val cs1 = ColumnConstraints()
+        cs1.percentWidth = 25.0
+        val cs2 = ColumnConstraints()
+        cs2.percentWidth = 25.0
+        val cs3 = ColumnConstraints()
+        cs3.percentWidth = 25.0
+        val cs4 = ColumnConstraints()
+        cs4.percentWidth = 25.0
+        columnConstraints.addAll(cs1, cs2, cs3, cs4)
 
-                                override suspend fun giveOrder(): Coordinate =
-                                    Coordinate(1, 1)
-                            },
-                            isHidden = false
-                        ))
-                        close()
+        prefWidth = 320.0
+        hgap = 10.0
+        vgap = 10.0
+        padding = insets(10.0)
+
+        row {
+            add(label("Player name:"), 0, 0, 2, 1)
+            add(textfield(nameProp), 2, 0, 2, 1)
+        }
+        row {
+            add(label("Settings:"), 0, 1, 4, 1)
+        }
+        row {
+            add(label("Width:"), 0, 2, 1, 1)
+            add(textfield(widthProp) {
+                widthProp.addListener { observable, oldValue, newValue ->
+                    println("old: $oldValue; new: $newValue")
+                    if (!newValue.matches(Regex("\\d*"))) {
+                        text = newValue.replace(Regex("\\d*"), "")
                     }
                 }
-                button("Cancel") {
-                    padding = insets(10.0)
-                    useMaxWidth = true
-                    action {
-                        close()
-                    }
+            }, 1, 2, 1, 1)
+            add(label("Height:"), 2, 2, 1, 1)
+            add(textfield(heightProp), 3, 2, 1, 1)
+        }
+        row {
+            add(button("Confirm") {
+                padding = insets(10.0)
+                useMaxWidth = true
+                action {
+                    continuation?.resume(Captain(
+                        object : CommanderController {
+                            override suspend fun getName(): String = nameProp.get()
+                            override suspend fun getRole(): Boolean = Random.nextBoolean()
+                            override suspend fun getSettings(): BattleSettings =
+                                BattleSettings(10, 10, listOf(4, 3, 2, 1))
+
+                            override suspend fun giveOrder(): Coordinate =
+                                Coordinate(1, 1)
+                        },
+                        isHidden = false
+                    ))
+                    close()
                 }
-            }
+            }, 0, 3, 2, 1)
+            add(button("Cancel") {
+                padding = insets(10.0)
+                useMaxWidth = true
+                action {
+                    close()
+                }
+            }, 2, 3, 2, 1)
         }
     }
 
